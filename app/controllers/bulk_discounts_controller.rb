857668ecd4e95e7df50dc0fe7ffbde477e717bd0
Cory Powell <coryrpowell@gmail.com>
2023-11-10 18:06:55 -0500

    Added testing for US 2 with passing tests and the beginning of testing for US 3.

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -8,4 +8,31 @@ class BulkDiscountsController < ApplicationController
      
   end
 
+  def new
+    find_merchant
+  end
+
+  def create
+    find_merchant
+    BulkDiscount.create!(percentage_discount: params[:percentage_discount],
+    quantity_threshold: params[:quantity_threshold],
+    merchant: @merchant)
+    redirect_to merchant_bulk_discounts_path(@merchant)
+
+  end
+
+
+  private
+  def bulk_discount_params
+    params.permit(:percentage_discount)
+  end
+
+  def find_bulk_discount_and_merchant
+    @bulk_discount = BulkDiscount.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def find_merchant
+    @merchant = Merchant.find(params[:merchant_id])
+  end
 end
