81bfdcf44ab3167333fc115a2759b39b986de82d
Cory Powell <coryrpowell@gmail.com>
2023-11-14 12:26:58 -0500

    added destroy all for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Transaction.destroy_all 
+Merchant.destroy_all 
+Item.destroy_all 
+Invoice.destroy_all 
+InvoiceItem.destroy_all 
+Customer.destroy_all 
+BulkDiscount.destroy_all 
+
 Rake::Task['db:drop'].invoke
 Rake::Task['db:create'].invoke
 Rake::Task['db:migrate'].invoke
