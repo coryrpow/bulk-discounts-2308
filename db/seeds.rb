2910d5d58303ce656179fdfee39937c397189224
Cory Powell <coryrpowell@gmail.com>
2023-11-14 12:17:39 -0500

    adding rake tasks for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Rake::Task['db:drop'].invoke
+Rake::Task['db:create'].invoke
+Rake::Task['db:migrate'].invoke
 
 Rake::Task["csv_load:all"].invoke
